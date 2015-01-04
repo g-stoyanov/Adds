ee6c91646c6dbe985ff59d2454b1c3694f6f3fb4
g-stoyanov <georgi.j.stoyanov@gmail.com>
2015-01-05 00:27:51 +0200

    Added filters, pagination and some other stuff.

--- AngularSpaAdds/app/js/services/adds.js
+++ AngularSpaAdds/app/js/services/adds.js
@@ -1,7 +1,7 @@
 ﻿angularSpaAdds.factory('addsData', function ($http, $log) {
     return {
-        getAllAdds: function (success) {
-            $http({ method: 'GET', url: 'http://softuni-ads.azurewebsites.net/api/ads?PageSize=10&StartPage=1' })
+        getAllAdds: function (success, categoryId, townId, startPage, pageSize) {
+            $http({ method: 'GET', url: 'http://softuni-ads.azurewebsites.net/api/Ads?CategoryId=' + categoryId + '&TownId=' + townId + '&StartPage=' + startPage + '&PageSize=' + pageSize })
             .success(function (data, status, headers, config) {
                 success(data);
             })
---
+++ AngularSpaAdds/app/js/services/towns.js
@@ -0,0 +1,13 @@
+﻿angularSpaAdds.factory('townsData', function ($http, $log) {
+    return {
+        getAllTowns: function (success) {
+            $http({ method: 'GET', url: 'http://softuni-ads.azurewebsites.net/api/towns' })
+            .success(function (data, status, headers, config) {
+                success(data);
+            })
+            .error(function (data, status, headers, config) {
+                $log.warn(data);
+            })
+        }
+    }
+})
