cdc02deccc3f10849990b86880c85827fed82855
g-stoyanov <georgi.j.stoyanov@gmail.com>
2015-01-04 16:09:07 +0200

    Added get all-ads logic and partial template.

---
+++ AngularSpaAdds/app/js/services/adds.js
@@ -0,0 +1,13 @@
+﻿angularSpaAdds.factory('addsData', function ($http, $log) {
+    return {
+        getAllAdds: function (success) {
+            $http({ method: 'GET', url: 'http://softuni-ads.azurewebsites.net/api/ads?PageSize=10&StartPage=1' })
+            .success(function (data, status, headers, config) {
+                success(data);
+            })
+            .error(function (data, status, headers, config) {
+                $log.warn(data);
+            })
+        }
+    }
+})
